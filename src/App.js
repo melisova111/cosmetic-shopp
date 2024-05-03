f9c739569fe220b34e351a350f25ea1c086dee4f
melisova111 <aytunukmirlanovna@gmail.com>
2024-05-03 15:35:15 +0600

    delete lorem in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-   <> Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae soluta debitis tempore nostrum quas! Totam, natus distinctio molestias vitae laborum ex a ipsum vero libero dolore facere, pariatur quos debitis facilis. Delectus dolore corrupti, iusto, distinctio placeat architecto quidem consequuntur impedit, corporis id sed sunt. Odit perspiciatis unde nulla culpa? Mollitia ad est expedita suscipit. Ratione ea, obcaecati maxime dolorum, libero quisquam ipsam praesentium quos incidunt facere quidem illo inventore fuga quaerat laboriosam animi. Voluptatum, adipisci eum! Corporis, dolorem delectus temporibus sint vero esse quae ducimus doloribus atque reprehenderit minima consequatur ex sequi aspernatur impedit totam, repellat nisi in obcaecati? Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Текста снова пояс свою единственное использовало путь что текст гор раз! Сбить, осталось она журчит предложения приставка безопасную, большого запятых пустился выйти заголовок повстречался языкового они, по всей снова дорогу страну рукопись всемогущая? Маленькая, если он агентство до пор толку переписывается? Там жаренные послушавшись большого вскоре текста залетают за сбить свою страна всемогущая толку она дал лучше осталось языкового запятых, проектах рукописи это по всей переписали. Большого журчит, последний однажды бросил снова заголовок предупредила меня букв от всех языкового пунктуация ему моей своих она, путь предупреждал его живет. Сих, маленькая! Подпоясал, сбить ты.</>
+   <> </>
   );
 }
 
