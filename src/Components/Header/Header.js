b5357f8bb2c623c192c9a713edd4290fea0e708e
melisova111 <aytunukmirlanovna@gmail.com>
2024-05-06 22:26:49 +0600

    delete the 2 route

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -49,7 +49,7 @@ function Header() {
                 <Route path="home" element={<Home />} />
                 <Route path="cart" element={<Cart />} />
                 <Route path="catalog" element={<Delivery />} />
-                
+                 
               </Routes>
 
             </Router>
