548ac8bad85d8ec2271a1256d91cd282023c6ab6
melisova111 <aytunukmirlanovna@gmail.com>
2024-05-08 14:41:04 +0600

    changed all sructure in header with app js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,35 @@ import './App.css';
 import './Components/Menu/Menu'
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
-import Delivery from './Pages/Delivery/Delivery';
-
+import {
+  BrowserRouter as Router,
+  Link,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Home from '../../Pages/Home/Home';
+import Cart from '../../Pages/Cart/Cart';
+import Delivery from '../../Pages/Delivery/Delivery';
+import NotFound from '../../Pages/NotFound/NotFound';
+import About from '../../Pages/About/About';
+import Contact from '../../Pages/Contact/Contact';
+import Order from '../../Pages/Order/Order';
 function App() {
   return (
-   <>
-<Header>
-</Header>
-
-
-
-<Footer></Footer>
+    <>
+      <Header>
+      </Header>
+      <Router>
+        <Routes>
+          <Route path="home" element={<Home />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="delivery" element={<Delivery />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="about" element={<About />} />
+          <Route path="order" element={<Order />} />
+        </Routes>
+      </Router>
+      <Footer></Footer>
     </>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,34 +1,23 @@
 import './Header.css';
-import {
-  BrowserRouter as Router,
-  Link,
-  Routes,
-  Route
-} from 'react-router-dom';
-import React from 'react';
-import Home from '../../Pages/Home/Home';
-import Cart from '../../Pages/Cart/Cart';
-import Delivery from '../../Pages/Delivery/Delivery';
-import NotFound from '../../Pages/NotFound/NotFound';
-import About from '../../Pages/About/About';
-import Contact from '../../Pages/Contact/Contact';
-import Order from '../../Pages/Order/Order';
 
+import React from 'react';
+
+import {
+  Link
+} from 'react-router-dom';
 function Header() {
   return (
     <>
 
-      <div className="all_menu
-">
+      <div className="all_menu">
         <div >
-          <Router>
             <nav className='menu flex' >
               <ul className='menu flex'>
                 <li>
                   <Link to="./Home">Home</Link>
                 </li>
                 <li>
-                  <Link to="./About">About</Link>
+                  <Link to="./About">Shop all</Link>
                 </li>
                 <li>
                   <Link to="./Delivery">Delivery</Link>
@@ -42,17 +31,6 @@ function Header() {
               </ul>
             </nav>
 
-
-            <Routes>
-              <Route path="home" element={<Home />} />
-              <Route path="cart" element={<Cart />} />
-              <Route path="delivery" element={<Delivery />} />
-              <Route path="contact" element={<Contact />} />
-              <Route path="about" element={<About />} />
-              <Route path="order" element={<Order />} />
-            </Routes>
-
-          </Router>
 
         </div>
       </div>
