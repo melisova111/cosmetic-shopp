89fc4880b642a7e6a51ea3643d0dc573d8b267b5
melisova111 <aytunukmirlanovna@gmail.com>
2024-05-08 14:17:52 +0600

    delete page named order

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -17,49 +17,46 @@ import Order from '../../Pages/Order/Order';
 function Header() {
   return (
     <>
-
-      <div className="all_menu
+    
+        <div className="all_menu
 ">
-        <div >
-          <Router>
-            <nav className='menu flex' >
-              <ul className='menu flex'>
-                <li>
-                  <Link to="./Home">Home</Link>
-                </li>
-                <li>
-                  <Link to="./About">About</Link>
-                </li>
-                <li>
-                  <Link to="./Delivery">Delivery</Link>
-                </li>
-                {/* <li>
-                    <Link to="./Order">Order</Link>
-                  </li> */}
-                <li>
-                  <Link to="./Contact">Contact</Link>
-                </li>
-                <li>
-                  <Link to="./Cart">Cart</Link>
-                </li>
-              </ul>
-            </nav>
+          <div >
+            <Router>
+              <nav className='menu flex' >
+                <ul className='menu flex'>
+                  <li>
+                    <Link to="./Home">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="./About">About</Link>
+                  </li>
+                  <li>
+                    <Link to="./Delivery">Delivery</Link>
+                  </li>
+                  <li>
+                    <Link to="./Contact">Contact</Link>
+                  </li>
+                  <li>
+                    <Link to="./Cart">Cart</Link>
+                  </li>
+                </ul>
+              </nav>
+            
 
+              <Routes>
+                <Route path="home" element={<Home />} />
+                <Route path="cart" element={<Cart />} />
+                <Route path="delivery" element={<Delivery />} />
+                <Route path="contact" element={<Contact />} />
+                <Route path="about" element={<About />} />
+                <Route path="order" element={<Order />} />
+              </Routes>
 
-            <Routes>
-              <Route path="home" element={<Home />} />
-              <Route path="cart" element={<Cart />} />
-              <Route path="delivery" element={<Delivery />} />
-              <Route path="contact" element={<Contact />} />
-              <Route path="about" element={<About />} />
-              <Route path="order" element={<Order />} />
-            </Routes>
+            </Router>
 
-          </Router>
-
+          </div>
         </div>
-      </div>
-
+      
     </>
   );
 }
